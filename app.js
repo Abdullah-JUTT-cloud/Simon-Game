d19df09c683b80ee450f0ace00f399db3f9d7739
Abdullah <abdullahjuttjutt910@gmail.com>
2025-12-03 03:02:40 +0500

    finale

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let gameSeq=[];
 let userSeq=[];
 let level=0;
+let highScore=0;
 let started=false;
+let h3=document.querySelector("h3");
 const boxColors=["red","yellow","green","purple"];
 let h2=document.querySelector("h2");
 document.addEventListener("keypress",function(){
@@ -25,9 +27,10 @@ function userflash(box){
     },300);
 }
 function levelUp(){
+    userSeq=[];
     level++;
-    h2.innerText=`Level ${level}`;
-    let randNum=Math.floor(Math.random()*3);
+    h2.innerText=`Level ${level} current score: ${level-1}`;
+    let randNum=Math.floor(Math.random()*4);
     let randColor=boxColors[randNum];
     let randBox=document.querySelector(`.${randColor}`);
     console.log(randNum);
@@ -39,16 +42,54 @@ function levelUp(){
     
 
 }
+function checkSeq(i){
+    console.log(`level: ${level}`);
+   
+    if(userSeq[i]===gameSeq[i]){
+        if(userSeq.length===gameSeq.length){
+            console.log("Level Up");
+            setTimeout(levelUp,1000);
+
+        }
+        console.log("Success");
+    }else{
+        let currscore=level-1;
+        if(currscore>highScore){
+            highScore=currscore;
+        }
+        h2.innerText="Game Over! Press any key to restart. high score: "+(level-1);
+        console.log(`gived up at level ${level}`);
+        document.querySelector("body").style.backgroundColor="red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor="";
+        },200);
+        h3.innerHTML=`HIGHEST SCORE TILL NOW: ${highScore}`;
+        reset();
+        
+    }
+}
 function boxClick(){
     let box=this;
     console.log(this);
     userflash(box);
+    useColor=box.getAttribute("id");
+    console.log(useColor);
+    userSeq.push(useColor);
+    console.log(userSeq);
+    checkSeq(userSeq.length-1);
 
 }
 let allBox=document.querySelectorAll(".box");
 for(box of allBox){
     box.addEventListener("click",boxClick);
 }
+function reset(){
+    gameSeq=[];
+    userSeq=[];
+    level=0;
+    started=false;
+}
+
 
 
 
